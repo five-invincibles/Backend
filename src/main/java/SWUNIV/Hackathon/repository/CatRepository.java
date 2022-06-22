c493506a083a307f4d750b1b014707c5006c0309
murry2018 <2clean8@naver.com>
2022-06-23 00:36:10 +0900

    add repository classes for each entity class

---
+++ src/main/java/SWUNIV/Hackathon/repository/CatRepository.java
@@ -0,0 +1,10 @@
+package SWUNIV.Hackathon.repository;
+
+import SWUNIV.Hackathon.entity.Cat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CatRepository extends JpaRepository<Cat, Long> {
+
+}
