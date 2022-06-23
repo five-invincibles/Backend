d91703d7983a10249eb4a9e33a1644652e696489
murry2018 <2clean8@naver.com>
2022-06-23 12:03:17 +0900

    add relational field involved in User entity.

--- src/main/java/SWUNIV/Hackathon/entity/User.java
+++ src/main/java/SWUNIV/Hackathon/entity/User.java
@@ -2,15 +2,17 @@ package SWUNIV.Hackathon.entity;
 
 import SWUNIV.Hackathon.enumerations.Authority;
 import SWUNIV.Hackathon.enumerations.University;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.Collection;
 
 @Entity
 @Builder
@@ -35,4 +37,9 @@ public class User extends BaseEntity{
     private University university;
 
     private String email;
+
+    @OneToMany(mappedBy = "modifier", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Collection<Picture> pictures;
+    @OneToMany(mappedBy = "modifier", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Collection<Article> articles;
 }
